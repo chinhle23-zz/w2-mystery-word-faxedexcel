639b0d998bf34b5a79e92092359c2f0875e2e104
faxedexcel <chinh.le23@gmail.com>
2019-02-22 14:40:13 -0500

    completed allowing user to play again

--- mystery_word.py
+++ mystery_word.py
@@ -66,7 +66,7 @@ def is_word_done(word, guesses):
     if correct_word == word:
         return True
     return False
-    
+  
 # read in text file
 with open('words.txt') as file:
     # 'with' statement is used to wrap the execution of a block with methods defined by a context manager https://docs.python.org/3.7/reference/compound_stmts.html#the-with-statement
@@ -99,63 +99,81 @@ easy_words = create_word_length_list(all_words, 4, 6)
 normal_words = create_word_length_list(all_words, 6, 8)
 hard_words = create_word_length_list(all_words, 8, 100)
 
-# ask user for difficulty level
-difficulty_level = input("Choose your level [Easy, Normal, or Hard]: ").casefold()
-invalid_difficulty = True
-while invalid_difficulty:
-    if difficulty_level == 'easy' or difficulty_level == 'normal' or difficulty_level == 'hard':
-        invalid_difficulty = False
-    else:
-        difficulty_level = input("Invalid input! Please type [Easy, Normal, or Hard]: ").casefold()
-
-# choose random word based on user input of easy, normal, or hard
-random_word = ''
-if difficulty_level == 'easy':
-    random_word = random.choice(easy_words)[0]
-        # 'random.choice(seq)' function is imported from the 'random' module' and returns a random element from the non-empty sequence 'seq'
-elif difficulty_level == 'normal':
-    random_word = random.choice(normal_words)[0]
-else:
-    random_word = random.choice(hard_words)[0]
-
-# start of the game: let the user know how many letters the computer's word contains
-print(f"""The mystery word contains {len(random_word)} characters.""")
-print(f"show mystery_word: {random_word}")
-letter_guess = demand_valid_letter(input("Guess any letter: "))
-
-already_guessed_list = []
-incorrect_guess_count = 0
-max_guess = 8
-continue_game = True
-while continue_game:
-
-    # if the letter has already been guessed, tell user to guess again  
-    if letter_guess in already_guessed_list:
-        letter_guess = demand_valid_letter(input(f"[{letter_guess}] already guessed. Try again: ")).upper()
-
-    # if the guessed letter is incorrect, let user know how many incorrect guesses they have left and tell user to guess again
-    elif letter_guess not in already_guessed_list and letter_guess not in random_word:
-        incorrect_guess_count += 1
-        already_guessed_list.append(letter_guess)
-        # if user has ran out of incorrect guesses, reveal the word and end the game
-        if incorrect_guess_count == max_guess:
-            print(f"Game Over. You lose! The word was: {random_word}")
-            continue_game = False
-        else:    
-            print(f"Sorry, [{letter_guess}] is not in the mystery word.")
-            letter_guess = demand_valid_letter(input(f"You get {max_guess - incorrect_guess_count} more incorrect guess(es). Try again: "))
-        
-    # if the guessed letter is correct, print the mystery word only displaying the correct guesses    
-    else:
-        already_guessed_list.append(letter_guess)
-        print(f"Correct guess! [{letter_guess}] is in the mystery word")
-        print_word(random_word, already_guessed_list)
-        # if all the letters in the mystery word have been guessed, let the user know and end the game
-        if is_word_done(random_word, already_guessed_list):
-            print(f"Congratulations! You guessed the mystery word: {random_word}")
-            break
+continue_mystery_word = True
+while continue_mystery_word:
+    # ask user for difficulty level
+    difficulty_level = input("Choose your level [Easy, Normal, or Hard]: ").casefold()
+    invalid_difficulty = True
+    while invalid_difficulty:
+        if difficulty_level == 'easy' or difficulty_level == 'normal' or difficulty_level == 'hard':
+            invalid_difficulty = False
         else:
-            letter_guess = demand_valid_letter(input("Guess another letter: "))  
+            difficulty_level = input("Invalid input! Please type [Easy, Normal, or Hard]: ").casefold()
+
+    # choose random word based on user input of easy, normal, or hard
+    random_word = ''
+    if difficulty_level == 'easy':
+        random_word = random.choice(easy_words)[0]
+            # 'random.choice(seq)' function is imported from the 'random' module' and returns a random element from the non-empty sequence 'seq'
+    elif difficulty_level == 'normal':
+        random_word = random.choice(normal_words)[0]
+    else:
+        random_word = random.choice(hard_words)[0]
+
+    # start of the game: let the user know how many letters the computer's word contains
+    print(f"""The mystery word contains {len(random_word)} characters.""")
+    print(f"show mystery_word: {random_word}")
+    letter_guess = demand_valid_letter(input("Guess any letter: "))
+    
+    already_guessed_list = []
+    incorrect_guess_count = 0
+    max_guess = 8
+    go_again_answer = ""
+    continue_round = True
+    while continue_round:
+
+        # if the letter has already been guessed, tell user to guess again  
+        if letter_guess in already_guessed_list:
+            letter_guess = demand_valid_letter(input(f"[{letter_guess}] already guessed. Try again: ")).upper()
+
+        # if the guessed letter is incorrect, let user know how many incorrect guesses they have left and tell user to guess again
+        elif letter_guess not in already_guessed_list and letter_guess not in random_word:
+            incorrect_guess_count += 1
+            already_guessed_list.append(letter_guess)
+            # if user has ran out of incorrect guesses, reveal the word and end the game
+            if incorrect_guess_count == max_guess:
+                print(f"Game Over. You lose! The word was: {random_word}")
+                
+                # ask user if they want to again: if yes then exit inner while loop, otherwise exit both inner and outer while loops
+                go_again_answer = input(f"Do you want to go again (Y/N)? ").upper()
+                if go_again_answer == 'Y':
+                    continue_round = False
+                else:
+                    continue_round = False
+                    continue_mystery_word = False
+            else:    
+                print(f"Sorry, [{letter_guess}] is not in the mystery word.")
+                letter_guess = demand_valid_letter(input(f"You get {max_guess - incorrect_guess_count} more incorrect guess(es). Try again: "))
+            
+        # if the guessed letter is correct, print the mystery word only displaying the correct guesses    
+        else:
+            already_guessed_list.append(letter_guess)
+            print(f"Correct guess! [{letter_guess}] is in the mystery word")
+            print_word(random_word, already_guessed_list)
+
+            # if all the letters in the mystery word have been guessed, let the user know and end the game
+            if is_word_done(random_word, already_guessed_list):
+                print(f"Congratulations! You guessed the mystery word: {random_word}")
+
+                # ask user if they want to again: if yes then exit inner while loop, otherwise exit both inner and outer while loops
+                go_again_answer = input(f"Do you want to go again (Y/N)? ").upper()
+                if go_again_answer == 'Y':
+                    continue_round = False
+                else:
+                    continue_round = False
+                    continue_mystery_word = False
+            else:
+                letter_guess = demand_valid_letter(input("Guess another letter: "))  
 
 print("Done")
 
